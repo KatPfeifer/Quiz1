6fbd183b5bd320fa86adbee4ddf9e45c1d02d327
katar <katar@128.4.123.236>
2018-09-15 12:28:23 -0400

    Final version

--- Quiz 1/src/pkgQuiz/quiz1.java
+++ Quiz 1/src/pkgQuiz/quiz1.java
@@ -4,35 +4,49 @@ import java.util.Scanner;
 
 public class quiz1 {
 	
-	Scanner sc = new Scanner(System.in);
-
-	public int battingAverage() {
+	static Scanner sc = new Scanner(System.in);
+	public static void BA(double H, double AB) {
+		double battingAverage=H/AB;
+		System.out.println("The Batting Average is " + battingAverage);
+	}
+	public static void TB(double R, double D, double T, double HR) {
+		double totalBases= R+D*2+T*3+HR*4;
+		System.out.println("The Total Bases is " + totalBases);
+	}
+	public static void SLG(double R, double D, double T, double HR, double AB) {
+		double slugging=(R+D*2+T*3+HR*4)/AB;
+		System.out.println("The Slugging Percentage is " + slugging);
+	}
+	public static void OBP(double H, double BB, double AB) {
+		double onBase=(H+BB)/(AB+BB);
+		System.out.println("The On-Base Percentage is " + onBase);
+	}
+	public static void OBS(double R, double D, double T, double HR, double H, double BB, double AB) {
+		double baseSlugging = ((R+D*2+T*3+HR*4)/AB)+((H+BB)/(AB+BB));
+		System.out.println("The On-Base + Slugging Percentage is " +baseSlugging);
+	}
+	public static void main(String[] args) {
+		System.out.println("Please enter player's name");
+		String playerName = sc.next();
+		System.out.println("Please enter At Bats");
+		double AB=sc.nextInt();
 		System.out.println("Please enter Hits");
-		int H = sc.nextInt();
-		System.out.println("Please enter At Bats");
-		int AB = sc.nextInt();
-		return H/AB;
-	}
-	int BA=battingAverage();
-	
-	public int totalBases() {
+		double H=sc.nextInt();
 		System.out.println("Please enter Runs");
-		int R = sc.nextInt();
+		double R=sc.nextInt();
 		System.out.println("Please enter Doubles");
-		int D = sc.nextInt();
+		double D=sc.nextInt();
 		System.out.println("Please enter Triples");
-		int T = sc.nextInt();
-		System.out.println("Please enter Home Runs");
-		int HR = sc.nextInt();
-		return R + 2*D + 3*T +4*HR;
+		double T=sc.nextInt();
+		System.out.println("Please enter HomeRuns");
+		double HR=sc.nextInt();
+		System.out.println("Please enter Walks");
+		double BB=sc.nextInt();
+		System.out.println("Here are the statistics for " + playerName);
+		quiz1.BA(H, AB);
+		quiz1.TB(R, D, T, HR);
+		quiz1.SLG(R,D,T,HR,AB);
+		quiz1.OBP(H,BB,AB);
+		quiz1.OBS(R,D,T,HR,H,BB,AB);
 	}
-	int TB=totalBases();
-	
-	public int slugging() {
-		System.out.println("Please enter At Bats");
-		int AB = sc.nextInt();
-		return TB/AB;
-	}
-	int SLG = slugging();
-	
-	}
+}
